3366080bbeed5c4f876bb8ad5cd9b92db0260658
spegisima <agneta.smergelyte@gmail.com>
2023-07-25 10:43:39 +0300

    added person movement

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const btnsElevatorFloor = document.querySelectorAll('.elevator-buttons > div');
 const elevator = document.querySelector('.elevator')
+const personBubble = document.querySelector('#to-where p');
+const personDiv = document.querySelector('.person-wrapper');
+
 
 let currentFloor = 1;
+let personInside = false;
+let personLocation = null;
+let personWish = null;
+
+const randomFloor = () => {
+    return Math.floor(Math.random()*3) + 1;
+}
+const randomChoice = (n1, n2) => {
+    if (Math.random() < 0.5) {
+        return n1;
+    } else return n2;
+}
+
+
+
+const newPerson = (n1, n2) => {
+    personLocation = randomChoice(n1, n2);
+    if (personLocation === 1) {
+        personWish = randomChoice(2,3);
+    } else if (personLocation === 2) {
+        personWish = randomChoice(1,3);
+    } else {
+        personWish = randomChoice(1,2);
+    }
+    personDiv.classList.add(`waiting-in-${personLocation}`)
+    personBubble.textContent = personWish;
+    personInside = false;
+}
+
+newPerson(2,3);
 
 
 //click events
@@ -11,11 +44,40 @@ let currentFloor = 1;
         if (currentFloor === 2) {
             elevator.classList.remove('second');
             elevator.classList.add('first');
+            if (personInside) {
+                personDiv.classList.remove('second');
+                personDiv.classList.add('first');
+            }
             currentFloor = 1;
         } else if (currentFloor === 3) {
             elevator.classList.remove('third');
             elevator.classList.add('first');
             currentFloor = 1;
+            if (personInside) {
+                personDiv.classList.remove('third');
+                personDiv.classList.add('first');
+            }
+        }
+        if (personInside && personWish === 1) {
+            setTimeout(() => {
+                personDiv.classList.remove('in-elevator');
+                personDiv.classList.add('leaving');
+                setTimeout(() => {
+                    personDiv.classList.remove('leaving', 'first');
+                    personDiv.style.display = 'none';
+                }, "1000");
+            }, "1000");
+            setTimeout(() => {
+                newPerson(2,3);
+                personDiv.style.display = 'flex';
+            }, "2300");
+        }
+        if (personDiv.classList.contains('waiting-in-1')) {
+            personInside = true;
+            setTimeout(() => {
+                personDiv.classList.remove('waiting-in-1');
+                personDiv.classList.add('in-elevator', 'first');
+            }, "1000");
         }
     }
     btnsElevatorFloor[1].onclick = () => {
@@ -23,11 +85,40 @@ let currentFloor = 1;
         if (currentFloor === 1) {
             elevator.classList.remove('first');
             elevator.classList.add('second');
+            if (personInside) {
+                personDiv.classList.remove('first');
+                personDiv.classList.add('second');
+            }
             currentFloor = 2;
         } else if (currentFloor === 3) {
             elevator.classList.remove('third');
             elevator.classList.add('second');
+            if (personInside) {
+                personDiv.classList.remove('third');
+                personDiv.classList.add('second');
+            }
             currentFloor = 2;
+        }
+        if (personInside && personWish === 2) {
+            setTimeout(() => {
+                personDiv.classList.remove('in-elevator');
+                personDiv.classList.add('leaving');
+                setTimeout(() => {
+                    personDiv.classList.remove('leaving', 'second');
+                    personDiv.style.display = 'none';
+                }, "1000");
+            }, "1000");
+            setTimeout(() => {
+                newPerson(1,3);
+                personDiv.style.display = 'flex';
+            }, "2300");
+        }
+        if (personDiv.classList.contains('waiting-in-2')) {
+            personInside = true;
+            setTimeout(() => {
+                personDiv.classList.remove('waiting-in-2');
+                personDiv.classList.add('in-elevator', 'second');
+            }, "1000");
         }
     }
     btnsElevatorFloor[0].onclick = () => {
@@ -35,12 +126,41 @@ let currentFloor = 1;
         if (currentFloor === 1) {
             elevator.classList.remove('first');
             elevator.classList.add('third');
+            if (personInside) {
+                personDiv.classList.remove('first');
+                personDiv.classList.add('third');
+            }
             currentFloor = 3;
         } else if (currentFloor === 2) {
             elevator.classList.remove('second');
             elevator.classList.add('third');
+            if (personInside) {
+                personDiv.classList.remove('second');
+                personDiv.classList.add('third');
+            }
             currentFloor = 3;
         }
-    }
+        if (personInside && personWish === 3) {
+            setTimeout(() => {
+                personDiv.classList.remove('in-elevator');
+                personDiv.classList.add('leaving');
+                setTimeout(() => {
+                    personDiv.classList.remove('leaving', 'third');
+                    personDiv.style.display = 'none';
+                }, "1000");
+            }, "1000");
+            setTimeout(() => {
+                newPerson(1,2);
+                personDiv.style.display = 'flex';
+            }, "2300");
+        }
+        if (personDiv.classList.contains('waiting-in-3')) {
+            personInside = true;
+            setTimeout(() => {
+                personDiv.classList.remove('waiting-in-3');
+                personDiv.classList.add('in-elevator', 'third');
+            }, "1000");
+        }
+     }
 }
 
